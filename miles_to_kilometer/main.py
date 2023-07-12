9be5fb99dee080d3c5f72bca6b1dfcdecbbfd5f0
Jeronimo Dcosta <dcosta.j17@gmail.com>
2023-07-11 21:57:35 -0400

    Completed the miles to kilometer app

--- miles_to_kilometer/main.py
+++ miles_to_kilometer/main.py
@@ -1,22 +1,40 @@
 from tkinter import *
 
 window = Tk()
-window.title("Tkinter program")
-window.minsize(width=500, height=300)
+window.title("Miles to KM converter")
+window.minsize(width=350, height=100)
+window.config(padx=20, pady=20)
 
-def button_click():
-    entry_text = entry.get()
-    label.config(text=entry_text)
+# User can input miles here
+miles_entry = Entry(text="0", width=10)
+miles_entry.grid(column=1, row=0)
 
+miles_label = Label(text="Miles", font=("System", 15))
+miles_label.config(padx=10)
+miles_label.grid(column=2, row=0)
 
-label = Label(text="This is some text", font=("Arial", 24, "bold"))
-label.pack()
+is_equal_to_label = Label(text="is equal to", font=("System", 15))
+is_equal_to_label.config(padx=10)
+is_equal_to_label.grid(column=0, row=1)
 
-button = Button(text="Click Me", command=button_click)
-button.pack()
+# Result is shown here
+km_result_label = Label(text="0", font=("System", 15))
+km_result_label.grid(column=1, row=1)
 
-entry = Entry()
-entry.pack()
+km_label = Label(text="Km", font=("System", 15))
+km_label.config(padx=10)
+km_label.grid(column=2, row=1)
+
+# Result calculation function
+def convert_to_km():
+    ''' This function converts miles inputed by the user to kilometers'''
+    miles = int(miles_entry.get())
+    km = round(miles * 1.60934, 3)
+    km_result_label.config(text=f"{str(km)}")
+
+# This button triggers the function
+calculate_button = Button(text="Calculate", command=convert_to_km)
+calculate_button.grid(column=1, row=2)
 
 
 window.mainloop()
