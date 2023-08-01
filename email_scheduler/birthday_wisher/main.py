e19583fe85ce5c02c05c76af9449672672ce5870
Jeronimo Dcosta <dcosta.j17@gmail.com>
2023-07-31 23:08:12 -0400

    Cleaning up the code

--- email_scheduler/birthday_wisher/main.py
+++ email_scheduler/birthday_wisher/main.py
@@ -49,17 +49,3 @@ with open("birthdays.csv") as file:
         if month == today_month and day == today_day:
             message_text = generate_birthday_message(name)
             send_birthday_message(message_text, email)
-
-        
-    
-
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
